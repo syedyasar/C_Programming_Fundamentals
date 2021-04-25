b7f7f212c7b0e441e7eed8d732690b2aa8a5e8fd
syedyasar <syedyasar.mjcet@gmail.com>
2021-04-24 17:10:45 -0700

    04_integer_demo.c

---
+++ 01_Introduction/04_integer_demo.c
@@ -0,0 +1,29 @@
+// int  demo  program
+#include <stdio.h>  //  This  is  necessary in view of  printf  function
+main()
+{
+	int    a  =  25;   // 'a' is variable with value  25
+	printf("a  :  %d \n "  ,  a);  //  a : 25 <next line>
+	//int  b  =  1,000;  //  Error   becoz  comma   is  not  allowed
+	//int  c  =  1,00,000;   //  Error   becoz  comma   is  not  allowed
+	//int  d  =  $75;  //  Error   becoz  $   is  not  allowed
+}
+
+
+
+/*
+Integer
+-------------
+1)  It  is  a  number  without  decimal  point
+
+2)  It  can  be  +ve, -ve  or  0
+     Eg  :
+	 a)  9247  ----> +ve  integer
+	 b)  -6248  ----> -ve  integer
+     c)  0
+
+3)  Special  characters  like  $  and   comma   are  not  allowed  in  the  number
+
+4)  %d  or  %i   format   is  used  for  integer
+*/
+
