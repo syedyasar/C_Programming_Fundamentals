7ea23f8f74c70879e324bcfbb39da53ed97597ac
syedyasar <syedyasar.mjcet@gmail.com>
2021-04-25 20:21:18 -0700

    04_Identify_Error

---
+++ 02_Constants_And_Variables/04_Identify_Error.c
@@ -0,0 +1,8 @@
+//  Identify Error
+main()
+{
+        int   x;  // okay
+        float   x;  // Error  becoz 'x'  is  already  int  variable 
+}
+
+// No  2  variables  can  have  same  name  i.e. Variable  name  must  be  unique
