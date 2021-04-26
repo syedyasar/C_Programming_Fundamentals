8beafd240a2aa753689e45df417c2cb871077445
syedyasar <syedyasar.mjcet@gmail.com>
2021-04-25 20:19:41 -0700

    03_Variable_Demo

---
+++ 02_Constants_And_Variables/03_Variable_Demo.c
@@ -0,0 +1,23 @@
+//  Variable  demo  program
+#include <stdio.h>
+main()
+{
+	int x = 10; // 'x' variable with  value  10
+	printf("x : %d \n" , x); // x  :  10
+	x = x + 5; // Variable  'x'  is  modified to 10 + 5 = 15  replacing 10
+	printf("x : %d \n" , x); // x :  15
+	x = x - 8; // variable 'x' is modified to 15 - 8 = 7 replacing 15
+	printf("x : %d \n" , x);  //  x : 7
+}
+
+/*
+Variable
+--------------
+1)  It  is  a  memory  cell  or  memory  location
+
+2)  It  can  hold  single   value
+
+3)  Value  of  the  variable  is  overwritten  every  time  a  new  value  is  represented  in  the  variable
+     i.e.   Variable  can  not   hold  more  than  one  value
+
+*/
