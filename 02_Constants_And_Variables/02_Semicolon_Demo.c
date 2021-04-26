092a73718bc5f3ee871e214358363a799ae9fd83
syedyasar <syedyasar.mjcet@gmail.com>
2021-04-25 20:16:01 -0700

    02_Semicolon_Demo

---
+++ 02_Constants_And_Variables/02_Semicolon_Demo.c
@@ -0,0 +1,19 @@
+//  Semicolon  (;) demo  program 
+#include <stdio.h>
+main()
+{
+	printf("Hyd\n") ; 	printf("Sec\n") ;   printf("Cyb\n") ;
+}
+
+
+
+
+/*
+;  opearator
+---------------------
+1)  ;  is  called statement  terminator
+
+2) There  must  be  ;  at  the  end  of  each  statement  in  the  program
+
+3)  Multiple  statements  can  be  in  the  same  line  provided  they  are  separated  by  semicolons 
+*/
