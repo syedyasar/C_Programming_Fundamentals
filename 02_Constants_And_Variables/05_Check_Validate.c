83d4fce69e6773bedfc3e3c2e3b338f6a92e13f9
syedyasar <syedyasar.mjcet@gmail.com>
2021-04-25 20:24:14 -0700

    05_Check_Validate

---
+++ 02_Constants_And_Variables/05_Check_Validate.c
@@ -0,0 +1,11 @@
+
+// Is  following  valid ?
+main()
+{
+        int   a; // 1st   variable  name  is  'a'
+        float  A; // 2nd  variable name is  'A'
+}
+
+
+//  int  a   and  float   A  are valid  becoz  they have different  names
+
