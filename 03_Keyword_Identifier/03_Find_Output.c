4acb22f1f93323d2bd3c96c23841d6ef0bb4b215
syedyasar <syedyasar.mjcet@gmail.com>
2021-04-27 06:37:12 -0700

    03_Find_Output

---
+++ 03_Keyword_Identifier/03_Find_Output.c
@@ -0,0 +1,11 @@
+// Find output
+#include <stdio.h>
+main()
+{
+	int  abcdefghijklmnopqrstuvwxyz1234567890  =  25;
+	printf("%d \n" , abcdefghijklmnopqrstuvwxyz1234567890); //  25
+	//printf("%d \n" , abcdefghijklmnopqrstuvwxyz12345); //  Error  becoz  full  name  is  required  but  not  31  characters
+}
+
+
+//  Full name is  required  but  not  31  characters even though  system  recognizes  31  characters
