b2eff4476907a0971608a0b18e8050da9bda01a2
syedyasar <syedyasar.mjcet@gmail.com>
2021-04-27 06:36:28 -0700

    02_Find_Output

---
+++ 03_Keyword_Identifier/02_Find_Output.c
@@ -0,0 +1,9 @@
+// Find output
+#include <stdio.h>
+main()
+{
+	int  sum = 10;  //  Okay  becoz  sum  is  a  user defined word
+	printf("sum : %d \n" , sum); // sum : 10
+	//int  auto = 20; //  Error  becoz  variable  name  can  not  be  a  keyword  like  auto
+}
+
