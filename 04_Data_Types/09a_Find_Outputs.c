28c11a91ba86cf2549e82df4d5e34bc97b4660fa
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-01 17:58:18 -0700

    09a_Find_Outputs

---
+++ 04_Data_Types/09a_Find_Outputs.c
@@ -0,0 +1,16 @@
+// Identify error
+#include<stdio.h>
+#include<stdbool.h>
+main()
+{ 
+//	string   s;   //   Error  becoz  string  data  type  does not exist
+	bool  b = true;   
+	bool  c = false;//  Error  becoz  boolean   data  type  does not exist
+	printf("%d\n",b);
+	printf("%d",c);
+} 
+
+
+// 'C'  does  not  support  string   data  type
+
+
