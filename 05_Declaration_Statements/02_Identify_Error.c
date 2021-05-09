2b1693510a7760ce2f4c3340879ab4a62a202474
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-09 13:34:57 -0700

    00_Declaration_Statement

---
+++ 05_Declaration_Statements/02_Identify_Error.c
@@ -0,0 +1,7 @@
+// Identify error
+#include <stdio.h>
+main()
+{
+		x = 25;  // error  becoz variable 'x' is not declared
+		int  x;
+}
---
+++ 05_Declaration_Statements/04b_Find_Outputs.c
@@ -0,0 +1,7 @@
+// Find  outputs
+#include <stdio.h>
+main()
+{
+		int   x;
+		printf("%d \n" , x); // Garbage value becoz variable 'x' is  not initialized
+}
