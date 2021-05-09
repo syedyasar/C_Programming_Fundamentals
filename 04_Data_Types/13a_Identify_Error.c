ad914c8addfdc788355d4e2a83128a1c377f323b
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-09 13:09:18 -0700

    13a_Identify_Error

---
+++ 04_Data_Types/13a_Identify_Error.c
@@ -0,0 +1,8 @@
+// Identify error
+#include <stdio.h>
+main()
+{
+		int  a  = 09248; // Error  becoz octal number can not contain 8 and 9
+		int  b  =  0A7B6;// Error  becoz octal number can not contain alphabets
+}
+
