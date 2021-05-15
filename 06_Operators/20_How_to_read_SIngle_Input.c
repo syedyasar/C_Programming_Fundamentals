529d4eecb3d6d57d7fc0d5636739d2ad26258dcc
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 16:04:10 -0700

    20_How_to_read_SIngle_Input

---
+++ 06_Operators/20_How_to_read_SIngle_Input.c
@@ -0,0 +1,14 @@
+
+// How  to  read  single  input ?
+#include  <stdio.h>
+main()
+{
+	int  x; 
+	printf("Enter an  integer input : ");
+	scanf("%i"  , &x);  //  25
+	printf("x : %d  \n" ,  x); // x : 25
+}
+
+
+//   Format  is   %d   or   %i    for   integer
+
