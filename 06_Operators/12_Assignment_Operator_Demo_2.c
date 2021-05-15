4954c639c2943fd2d5faaf83252d953775b7feb6
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 15:58:51 -0700

    12_Assignment_Operator_Demo_2

---
+++ 06_Operators/12_Assignment_Operator_Demo_2.c
@@ -0,0 +1,37 @@
+// Identify error
+#include <stdio.h>
+main()
+{
+	int  a = 1 , b = 2 , c = 3 , d = 4;
+	25 = a;  // Error  becoz  op1  is  value
+    a + b = c + d;  //  Error  becoz  op1 is an expression
+}
+
+
+
+
+
+
+
+/*
+
+//  op1  =  op2;  ---> op2 can be  value, variable or expression but op1 must be variable only
+
+
+                =												==
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1)  =  is  assignment  operator						1)  ==  is  relational  operator
+
+2)  a = 7  means    7  is  assigned  to  variable  'a'                2)  a == 7  compares  value  of  variable  'a'  with 7
+     and  result  of  a = 7  is  also  7						     and  result  is  true  or  false
+
+3)  a = b  and  b = a  are  different						3)  a == b  and  b == a  are  same
+
+4)  op1 = op2										4)  op1  ==  op2
+     op1  must    be  variable  only		                            op1   can  be  anything  i.e. value , variable or expression
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+=  means  assignment
+==  means  comparison
+
+
+*/
