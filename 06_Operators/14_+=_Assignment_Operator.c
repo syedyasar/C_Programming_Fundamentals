47293d98ebb22171facdbb27a0d174f61e575f1c
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 16:00:14 -0700

    14_+=_Assignment_Operator

---
+++ 06_Operators/14_+=_Assignment_Operator.c
@@ -0,0 +1,22 @@
+
+// +=  operator  demo program
+#include <stdio.h>
+main()
+{
+	int  a = 7;
+	printf("a : %d  \n" ,  a); // 7
+	a += 5;  //  5  is  added  to variable  'a'  and  'a'  becomes  12
+	printf("a : %d  \n" ,  a); // 12
+}
+
+
+
+
+/*
+1)  a = 7;
+     a += 5;  --->  5  is  added  to  variable  a.  so  a  is  12
+     a += 5  is  interpreted  as  a = a + (5)
+
+2)   operand1  op=  operand2    -->  operand1  =  operand1  op  (operand2)   where  op  is  any operator
+*/
+
