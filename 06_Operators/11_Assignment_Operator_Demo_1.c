48ff9a73e7f99e00b1a168b821812e70646d5942
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 15:58:19 -0700

    11_Assignment_Operator_Demo_1

---
+++ 06_Operators/11_Assignment_Operator_Demo_1.c
@@ -0,0 +1,12 @@
+// Assignment  operators demo program1
+#include <stdio.h>
+main()
+{
+	int  a , b , c;
+	printf(" %d  \n" , a = 25); // 25
+	printf(" %d  \n" ,  b = a);  //  25
+	printf(" %d  \n" ,  c = a + b * 3);  //  100
+}
+
+
+
