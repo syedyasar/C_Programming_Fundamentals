e7c7b4317a0f6db795a667aa14bfc525bb70669e
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 12:49:00 -0700

    64_Even_Odd_Check_Method2

---
+++ 06_Operators/64_Even_Odd_Check_Method2.c
@@ -0,0 +1,13 @@
+// Same program in another way
+#include<stdio.h>
+int  main()
+{
+        int  x;
+        printf("Enter  any  number  :  ");
+        scanf("%d" , &x); 
+		x % 2  ? printf("Odd Number\n") : printf("Even Number \n");
+}
+
+
+
+
