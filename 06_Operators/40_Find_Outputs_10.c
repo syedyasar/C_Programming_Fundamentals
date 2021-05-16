fbe1ad90ffc35be8acd84eadea0a99ef5da5ece5
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 21:46:40 -0700

    40_Find_Outputs_10

---
+++ 06_Operators/40_Find_Outputs_10.c
@@ -0,0 +1,51 @@
+// Find output
+#include <stdio.h>
+main()
+{
+	
+	printf("%%");  //   %
+	printf("\n");  //  moves  to  next  line
+	printf("   \"  "); //  "
+	printf("\n"); // moves to  next line
+	printf("\\n"); //  \n
+	printf("\n");
+	printf(" \"  C  \"   is    a   great  language \n");  // "C"  is   a   great  language
+	printf("%%d\n"); //   %d
+	printf("\\");   //   
+	printf("\n");
+	
+}
+
+/*
+1)  How  to   print  "  ?   ---->  \"
+
+2)  How  to   print  %   ?   ---->   %%
+
+3)  How  to   print  \   ?   ---->   \\
+
+4)  How  to   print  \n   ?   ---->   \\n
+
+5)  How  to   print  %d   ?   ---->   %%d
+
+6)  printf("/");   ---->  /
+     printf("//");   ---->  //
+     printf("&");   ---->  &
+     printf("&&");   ---->  &&
+
+
+		scanf()										printf()
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1) It  is  input  function								1)  It  is  output  function
+
+2) It  reads  inputs  from  keyboard  to  memory variables  2)  It  writes  values  of  variables  to  monitor
+
+3) It  is  call-by-address  function.  	                                3)  It  is  call-by-value  function.
+    Hence  & is  mandatory   in  scanf					     Hence  &  is  not  rquired  in  printf
+
+4) It  can  takes only  variables along  with  &                     4)  It  can  take  variable , expression  and  msg
+
+5) It  deals  with  input  device  (keyboard)
+                          5)  It  deals  with  output  device(monitor)
+
+
+*/
