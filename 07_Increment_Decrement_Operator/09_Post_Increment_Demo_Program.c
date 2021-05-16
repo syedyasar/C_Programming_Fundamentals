7889a9cc73cf5401ae44dbe9b5a771beb8c0215f
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 14:10:48 -0700

    09_Post_Increment_Demo_Program

---
+++ 07_Increment_Decrement_Operator/09_Post_Increment_Demo_Program.c
@@ -0,0 +1,8 @@
+// post increment demo program
+#include<stdio.h>
+int main()
+{
+	int  a = 7;  
+	printf("%d \n" , a++); // Result of  a++  is  old  value  7  and  7  is  printed
+	printf("%d \n" , a); // 8 becoz  variable 'a' is  already  incremented
+}
