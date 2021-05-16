e5fcd930d4a4f20266ac1ba35b14424ffee600e7
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 14:15:17 -0700

    01_Find_Outputs

---
+++ 07_Increment_Decrement_Operator/01_Find_Outputs.c
@@ -0,0 +1,11 @@
+//  Find  outputs
+#include  <stdio.h>
+main()
+{
+	int  a = 25;
+	++a; // increments  variable  'a'   by   1
+	printf("a  :  %d \n" , a); // 26
+}
+
+
+//  ++a ,  a++  ,  a+=1  and  a=a+1  are  same
