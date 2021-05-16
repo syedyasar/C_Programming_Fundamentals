0903ba6eb05937a7976c1bdd8c4c5c19ae2a8a25
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 14:16:08 -0700

    02_Find_Outputs

---
+++ 07_Increment_Decrement_Operator/02_Find_Outputs.c
@@ -0,0 +1,10 @@
+// Find outputs
+# include <stdio.h>
+main()
+{
+	int  a = 25;
+	--a;   // decrements  variable 'a'  by  1
+	printf("a : %d \n" , a); //  24
+}
+
+// --a  ,  a--  ,  a-=1  and  a = a -  1  are  same
