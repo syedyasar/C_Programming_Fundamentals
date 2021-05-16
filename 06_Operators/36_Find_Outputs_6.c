7bcba27cfe64042428c4dd31c874199f6541d5b9
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 21:43:22 -0700

    36_Find_Outputs_6

---
+++ 06_Operators/36_Find_Outputs_6.c
@@ -0,0 +1,19 @@
+// Find output
+#include <stdio.h>
+main()
+{
+	char  a[]  = "Rama Rao"; //  Array  is  intialized with string
+    printf("%s \n" , a); // string in  array  'a'  is  printed  until  '\0'   ----> Rama Rao
+}
+
+
+
+/*
+1)  int  -   %d
+    float   -   %f
+	char  -   %c
+	 ----    -     %s
+
+2) There is no string datatype but %s format exists
+*/
+
