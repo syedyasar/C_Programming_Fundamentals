6a5008c54c97461cd710be6c3bff1cb8a51e6d55
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 14:11:02 -0700

    10_Pre_Increment_Demo

---
+++ 07_Increment_Decrement_Operator/10_Pre_Increment_Demo.c
@@ -0,0 +1,8 @@
+// preincrement demo program
+#include<stdio.h>
+int main()
+{
+		int  a = 7; 
+		printf("%d\n" , ++a); // Result  of  ++a  is  new  value  8
+		printf("%d \n" , a);//  8  becoz  variable  'a'  is  already  incremented
+}
