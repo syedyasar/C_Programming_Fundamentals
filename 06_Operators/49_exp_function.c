51c20e590af24a3618f47158a4f8d80233f0f7d0
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 21:51:00 -0700

    49_exp_function

---
+++ 06_Operators/49_exp_function.c
@@ -0,0 +1,13 @@
+//   exp  function  demo  program
+#include "stdio.h"
+#include "math.h"
+main()
+{
+		printf("%g \n" , exp(1));  //   e ^ 1 = 2.71828
+		printf("%g \n" , exp(2));  //  e ^ 2 
+		printf("%g \n" , exp(-2)); // e ^ -2
+		printf("%g \n" , exp(0));  // e ^ 0 = 1
+}
+
+
+// where  e   value  is   2.71828
