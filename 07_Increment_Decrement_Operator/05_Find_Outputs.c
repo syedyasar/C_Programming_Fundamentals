70019da5e98e78142d62bff26693b8ff79662a34
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 14:08:15 -0700

    05_Find_Outputs

---
+++ 07_Increment_Decrement_Operator/05_Find_Outputs.c
@@ -0,0 +1,9 @@
+// Find outputs
+# include <stdio.h>
+main()
+{
+	char  ch = 'Z'; //  90
+	ch++; // Ascii  value is  incremented
+	printf("ch : %c \n" , ch);  //  that  char whose ascii  value is  91  ---> [
+	printf("ch : %d \n" , ch);  //  91
+}
