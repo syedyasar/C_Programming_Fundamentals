2b3bf047193f75075e7c7f580453282779c40e7f
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 14:17:38 -0700

    04_Find_Outputs

---
+++ 07_Increment_Decrement_Operator/04_Find_Outputs.c
@@ -0,0 +1,11 @@
+// Find outputs
+# include <stdio.h>
+main()
+{
+	char  ch = 'B'; //  66
+	ch++;  // Ascii  value  is  incremented  i.e. 67
+	printf("ch : %c \n" , ch); //  C
+	printf("ch : %d \n" , ch); //  67
+}
+
+//  char  variable can  be  incremented  but  Ascii value  is  incremented
