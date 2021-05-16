47ac10284f2b8deaa6f65fb531f0e5ac76f76122
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 14:08:29 -0700

    06_Find_Outputs

---
+++ 07_Increment_Decrement_Operator/06_Find_Outputs.c
@@ -0,0 +1,10 @@
+// Find outputs
+# include <stdio.h>
+main()
+{
+	char  ch = 'A'; // 65
+	ch--; //  Ascii  value is  decremented 
+	printf("ch : %c \n" , ch);  //  that  char  whose ascii  value is 64  ---> @
+	printf("ch : %d \n" , ch);  //  64
+}
+
