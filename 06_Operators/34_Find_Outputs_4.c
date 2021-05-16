e10252b75952788f48ef19a944102294aaf4307c
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-15 19:10:42 -0700

    34_Find_Outputs_4

---
+++ 06_Operators/34_Find_Outputs_4.c
@@ -0,0 +1,10 @@
+// Find  outputs
+#include <stdio.h>
+main()
+{
+	int   x = 25;
+	float  y = 10.8;
+	char  ch = 'g';
+	printf("%d  "  "%g  "   "%c  "   "\n"    , x , y , ch); // 25   10.8     g
+}
+
