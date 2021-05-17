0c218fc96ec045c8a116c4ce75fbcc0b003f92a9
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 21:55:04 -0700

    11_Comma_Operator

---
+++ 08_Conditional_Statements/11_Comma_Operator.c
@@ -0,0 +1,14 @@
+// ,  operator  demo  program  
+#include<stdio.h>
+int main()
+{
+	int  i = 2;
+	if( i == (1 , 2)) 
+		printf("Hyd\n");
+	else
+		printf("Sec\n");
+}
+
+
+// op1 , op2  = op2
+// op1, op2, op3 = op2 , op3 = op3
