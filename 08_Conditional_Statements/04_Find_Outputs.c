26e56d63634e404651d95fe298a92b4f2b3aa50c
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 21:53:44 -0700

    04_Find_Outputs

---
+++ 08_Conditional_Statements/04_Find_Outputs.c
@@ -0,0 +1,19 @@
+// Find output
+#include<stdio.h>
+main()
+{
+    int  a = 0;
+	if(a =   -7) 
+			printf("Hyd \n");
+    else
+			printf("Sec  \n");
+}
+
+/*
+      Condition    output
+	  --------------------------------
+	    a = 0             Sec
+	    a == 0           Hyd
+	    a = 7             Hyd
+	    a = -5            Hyd
+*/
