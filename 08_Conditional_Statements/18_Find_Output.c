5c1f2a3b88edb52382e9e32c406dc49b0f171d06
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 21:56:37 -0700

    18_Find_Output

---
+++ 08_Conditional_Statements/18_Find_Output.c
@@ -0,0 +1,12 @@
+
+//Find Outputs
+
+#include<stdio.h>
+main()
+{
+		int i = 0, j = 1, k = 0;
+		if(++k, j, i++) 
+				printf("if :  %d %d %d \n", i, j, k);
+		else
+				printf("else : %d %d %d \n", i, j, k); 
+}
