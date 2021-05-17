7953040a4aebde9e0e776466457e974c65216dfe
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-16 21:55:48 -0700

    13_Find_Output

---
+++ 08_Conditional_Statements/13_Find_Output.c
@@ -0,0 +1,23 @@
+// Find output
+#include<stdio.h>
+main()
+{
+		int  i = 5;
+        if(i = i - 5 > 4)  
+			printf("Hyd\n");
+        else
+			printf("Sec  :  %d \n", i);
+}
+
+
+
+/*
+1) Arithmetic operators(+ , - , * , / , %)  ---> Highest priority
+
+2) Relational  operators(> ,  <  ,  == , ....)  ---> 2nd Highest priority
+
+3) Assignment opeartors (= , += , -= , ....)  ---> Lowest Priority
+
+
+4) i = i - 5 > 4  ---> i = 0 > 4 ----> i = 0  ---> 0
+*/
