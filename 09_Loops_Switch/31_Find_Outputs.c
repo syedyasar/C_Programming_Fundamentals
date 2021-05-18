235eac1f5c908b21dae6632443850e33a5f704a4
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-17 17:22:29 -0700

    31_Find_Outputs

---
+++ 09_Loops_Switch/31_Find_Outputs.c
@@ -0,0 +1,10 @@
+// Find outputs
+#include <stdio.h>
+main()
+{
+		while(0) // Not  executed 
+			printf("Hello\n");
+        printf("Bye\n");
+}
+
+//  while(false)  is  okay  provided  stdbool.h  is  included
