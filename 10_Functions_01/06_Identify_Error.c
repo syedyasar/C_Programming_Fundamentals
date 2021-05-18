f3ac1b3c0ef2fd7fd0701949e18c5e8e1058156b
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-17 19:04:34 -0700

    06_Identify_Error

---
+++ 10_Functions_01/06_Identify_Error.c
@@ -0,0 +1,19 @@
+//  Identify  error
+int  f1()
+{
+}
+float  f1() //  Error  becoz  f1  function is  already  defined
+{
+}
+void  auto() // Error  becoz  auto  is  a keyword
+{
+}
+main()
+{
+}
+
+/*
+1)  No  2  functions can  have  same name and function  name must be unique
+
+2) Function name  must  be  a  user defined word  but  not  keyword
+*/
