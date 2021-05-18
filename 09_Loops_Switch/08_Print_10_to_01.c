3cdd79d02229017008035b01211d8dc01c3dbb56
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-17 17:14:04 -0700

    08_Print_10_to_01

---
+++ 09_Loops_Switch/08_Print_10_to_01.c
@@ -0,0 +1,16 @@
+//  Print  10  ,  9  ,   8  ,  ..... 1
+#include<stdio.h>
+main()
+{
+	int   i;
+	for(i = 10 ; i >= 1 ; i--)
+		printf("%d \n" , i); //  prints  'i'  whre  i  varies  from 10  downto  1  in steps  of  -1
+}
+
+
+/*
+ 1) Use <= and ++ for increasing order
+
+ 2) Use >= and -- for decreasing order
+
+ */
