ea538041bc009f3f17ab066b64b536920e751cd4
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-17 17:21:28 -0700

    25_FInd_Outputs

---
+++ 09_Loops_Switch/25_FInd_Outputs.c
@@ -0,0 +1,19 @@
+//  Find   outputs
+#include <stdio.h>
+main()
+{
+    for(;;)  //  2  semicolons  are mandatory in for loop
+			printf("Hello\n");
+	printf("Bye\n"); // Not  executed due to  infinite loop
+}
+
+
+
+/*
+1) When no condition is specified in for loop, default is true
+
+
+2) for loop becomes infinite and Hello is displayed infinite number of times
+
+*/
+
