e2138972456597ce2f422a8e0a778504f0a492c9
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-21 20:50:33 -0700

    16_FInd_Outputs

---
+++ 12_Bitwise_Operator/16_FInd_Outputs.c
@@ -0,0 +1,11 @@
+// Find  outputs
+#include <stdio.h>
+main()
+{
+		int  a;
+		printf("Enter any  number  :  ");
+		int  x = scanf("%d" , &a );  //  scanf  reads  single  input  and  returns   1
+		printf("x : %d \n"  , x ); //   1
+		printf("a : %d \n"  , a ); //  user  input
+}
+
