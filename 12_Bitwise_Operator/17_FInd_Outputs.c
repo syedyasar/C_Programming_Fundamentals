6c38d7f557e6ed6009dab58f6141b2a7708397ea
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-21 20:50:41 -0700

    17_FInd_Outputs

---
+++ 12_Bitwise_Operator/17_FInd_Outputs.c
@@ -0,0 +1,15 @@
+// Find  outputs
+#include <stdio.h>
+main()
+{
+		int  a , b , c;
+		printf("Enter any  3  numbers \n");
+		printf("%d\n" , scanf("%d %d %d" , &a , &b , &c));  //  3
+}
+
+
+/*
+1)  inner function scanf reads  3 inputs and returns 3
+
+2)  printf becomes printf("%d\n" , 3); ---> 3
+*/
