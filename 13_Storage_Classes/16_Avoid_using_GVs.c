0ec76318c7ac0986918f12c7f703d92e90d8e3d3
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-23 19:14:40 -0700

    16_Avoid_using_GVs

---
+++ 13_Storage_Classes/16_Avoid_using_GVs.c
@@ -0,0 +1,24 @@
+// Avoid    using    GV's
+int  a ,  b; // Gv's
+void  f1()
+{
+}
+void  f2()
+{
+}
+void  f3()
+{
+}
+main()
+{
+}
+
+
+/*
+1)  Using  Gv's  is  a  bad  programming  becoz  all  the  functions  will  have  same  signature
+
+2)  main , f1  , f2  and  f3   have  no  parameters  becoz   parameter  passing  is  not  required  for  Gv's
+
+3) All  the  functions   are  void   function   becoz   return  is  not  required  for  Gv's
+*/
+
