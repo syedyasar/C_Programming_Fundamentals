76af4fb2174c6ab1d06944bc197225cd52ef29f0
syedyasar <syedyasar.mjcet@gmail.com>
2021-05-30 15:46:10 -0700

    28_Constant_array

---
+++ 19_Arrays/28_Constant_array.c
@@ -0,0 +1,10 @@
+// constant  array  demo program
+#include <stdio.h>
+main()
+{
+		const int    a[] = {10 , 20 , 15 , 18}; //  'a'  is  a  constant  array
+        a[0]++;  //  Error     becoz   elements  of   array  can  not  be  modified
+}
+
+
+//  Elements of  array  can not  be  modified becoz it is constant  array
