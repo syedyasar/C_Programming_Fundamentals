2cbb131aeaf070d705be408e8b17cb19d233c2d6
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-12 15:15:26 -0700

    05_Identify_Error

---
+++ 30_2D_Arrays/05_Identify_Error.c
@@ -0,0 +1,10 @@
+// Identify  error
+#include <stdio.h>
+main()
+{
+	int  a[3][4];
+    a  = {10 , 20 , 30 , 40 , 50 , 60 , 70 ,80 , 90 , 100 ,  110 , 120};  //  Error  becoz  matrix  can  not  be initialized  later
+}
+
+
+//  Matrix  can   be   initialized  at  the  time  of  declaration  but  not   afterwards
