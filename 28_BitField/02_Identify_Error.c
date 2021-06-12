8f97733b478181c3ef99e85ce69ad3c64f8a0315
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-12 11:08:53 -0700

    02_Identify_Error

---
+++ 28_BitField/02_Identify_Error.c
@@ -0,0 +1,13 @@
+// Identify error
+struct s1
+{
+	float  a : 3; //  Error  becoz  float  is  not  allowed
+	double  b : 4; //  Error  becoz  double  is  not  allowed
+};
+main()
+{
+}
+
+
+
+// Bit-field type  can be anything except float and double
