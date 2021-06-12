21554053a2e2aefba19de5b5bed9ee800831beaf
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-12 00:21:34 -0700

    02_Identify_Error

---
+++ 25_Structure/02_Identify_Error.c
@@ -0,0 +1,11 @@
+// Identify  Error
+struct  s1
+{
+	int  x = 25; // Error  becoz  memory  is  not  allocated  to member  'x'
+	float  y = 10.8; // Error  becoz  memory  is  not  allocated  to member  'y'
+}
+main()
+{
+}
+
+//  Members  can  not  be  initialized  inside  structure  definition  becoz    memory  is  not  allocated
