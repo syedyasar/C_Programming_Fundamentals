dae89fe57bcfc7338cb792a72bd63294b03ef3ca
syedyasar <syedyasar.mjcet@gmail.com>
2021-06-12 10:45:07 -0700

    04_Identify_Error

---
+++ 27_eNUM/04_Identify_Error.c
@@ -0,0 +1,13 @@
+// Identify  Error
+enum  month  {Jan = 1 , Feb , Mar , Apr , May , Jun , Jul , Aug , Sep , Oct , Nov , Dec};
+enum  num_sys  {Bin = 2 , Oct = 8 , Dec = 10 , Hex = 16};
+main()
+{
+}
+
+
+/*
+1) 2 enumerators can not have same name even though they belong to different  enumerator lists.
+
+2) Oct  and  Dec exist in both the enumerator lists which is not permitted
+*/
